44142a2412eeb7cca3166e26a3556a243edd28de
Dmitri Khrebtukov <khr128@gmail.com>
2011-07-08 08:30:59 -0700

    Add specs for point and linear interpolation

--- lib/rumerical.rb
+++ lib/rumerical.rb
@@ -4,3 +4,6 @@ $:.unshift(File.dirname(__FILE__)) unless
 module Rumerical
   VERSION = '0.0.1'
 end
+
+require 'point/point'
+require 'interpolation/linear'
--- spec/interpolation_spec.rb
+++ spec/interpolation_spec.rb
@@ -2,10 +2,29 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 # Time to add your specs!
 # http://rspec.info/
-describe "Place your specs here" do
+describe "Linear interpolation" do
   
-  it "find this spec in spec directory" do
-    # violated "Be sure to write your specs"
+  it "should interpolate between two points" do
+    p1 = Rumerical::Point.new(1.0, 2.0)
+    p2 = Rumerical::Point.new(2.0, 4.0)
+    interpol = Rumerical::Interpolation::Linear.new(p1, p2)
+    interpol.should_not be_nil
+
+    ptest1 = Rumerical::Point.new(1.5, 3.0)
+    ptest2 = Rumerical::Point.new(1.8, 3.6)
+    interpol[ptest1.x].should == ptest1.y
+    interpol[ptest2.x].should == ptest2.y
+
+   #Different points 
+    p1 = Rumerical::Point.new(1.0, 3.0)
+    p2 = Rumerical::Point.new(2.0, 6.0)
+    interpol = Rumerical::Interpolation::Linear.new(p1, p2)
+    interpol.should_not be_nil
+
+    ptest1 = Rumerical::Point.new(1.5, 4.5)
+    ptest2 = Rumerical::Point.new(1.8, 5.4)
+    interpol[ptest1.x].should == ptest1.y
+    interpol[ptest2.x].should == ptest2.y
   end
   
 end
