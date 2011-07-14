37e38c81ea8ffbde7daa49fe09b75470293d9538
Dmitri Khrebtukov <khr128@gmail.com>
2011-07-14 16:04:42 -0400

    Add exception spec.

--- spec/linear_algebra/gauss_jordan_spec.rb
+++ spec/linear_algebra/gauss_jordan_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe "Gauss-Jordan elimination" do
-
+  it "should raise exception if matrix is singular" do
+    mi = {
+      1 => { 1=>1, 2=>1, 3=>1},
+      2 => { 1=>1, 2=>1, 3=>1},
+      3 => { 1=>1, 2=>1, 3=>1},
+    }
+    m = Rumerical::Matrix.new(mi)
+    lambda{m.gaussj(m)}.should raise_error(RuntimeError, "gaussj: Singular matrix")
+  end
 end
